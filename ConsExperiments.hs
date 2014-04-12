c7e68cc9f410570c632eeb93c0fe63306eeeb5ab
Kenneth Foner <kenny.foner@gmail.com>
2014-04-12 13:11:30 -0400

    Working version of freeze; removed legacy code.

--- ConsExperiments.hs
+++ ConsExperiments.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveFunctor
            , DeriveFoldable
-           , DeriveTraversable #-}
+           , DeriveTraversable
+           , FlexibleContexts #-}
 
 module ConsExperiments where
 
@@ -16,12 +17,18 @@ import qualified Data.Map as M
 
 import Control.Monad
 import Control.Applicative
+import Control.Arrow
+
 import Data.Traversable
 import Data.Foldable
 import Data.Monoid
+import Data.Functor
 
 import Data.Fix
 import Data.Functor.Compose
+
+instance (Show (f (g a))) => Show (Compose f g a) where
+    show = show . getCompose
 
 data Distinct x = Distinct Unique x
    deriving ( Functor , Foldable , Traversable , Eq , Ord )
@@ -45,126 +52,44 @@ instance MonadUnique (ST s) where
 data Pair x = Pair { car :: x , cdr :: x }
    deriving ( Functor , Foldable , Traversable , Show , Eq , Ord )
 
+setCar, setCdr ::  x -> Pair x -> Pair x
+setCar a' (Pair a b) = Pair a' b
+setCdr b' (Pair a b) = Pair a  b'
+
 instance Applicative Pair where
    pure x = Pair x x
    (Pair f g) <*> (Pair a b) = Pair (f a) (g b)
 
-unFixCompose3 :: Fix (Compose (Compose (Compose f g) h) i)
-  -> f (g (h (i (Fix (Compose (Compose (Compose f g) h) i)))))
-unFixCompose3 = getCompose . getCompose . getCompose . unFix
+type RefStruct s f = Fix (Compose (Compose Distinct (STRef s)) f)
 
-fixCompose3 :: f (g (h (i (Fix (Compose (Compose (Compose f g) h) i)))))
-                        -> Fix (Compose (Compose (Compose f g) h) i)
-fixCompose3 = Fix . Compose . Compose . Compose
+unFixCompose2 :: Fix (Compose (Compose f g) h) -> f (g (h (Fix (Compose (Compose f g) h))))
+unFixCompose2 = getCompose . getCompose . unFix
 
-type RefStruct s f g = Fix (Compose (Compose (Compose Distinct f) (STRef s)) g)
+new :: f (RefStruct s f) -> ST s (RefStruct s f)
+new = (Fix . Compose . Compose <$>) . (distinguish =<<) . newSTRef
 
-makeStructure :: Traversable f => f (g (RefStruct s f g)) -> ST s (RefStruct s f g)
-makeStructure = (fixCompose3 <$>) . (distinguish =<<) . traverse newSTRef
+modify :: RefStruct s f -> (f (RefStruct s f) -> f (RefStruct s f)) -> ST s ()
+modify = modifySTRef . undistinguish . unFixCompose2
 
-modifyRefAt :: (f (STRef s (g (RefStruct s f g))) -> STRef s (g (RefStruct s f g)))
-                      -> (g (RefStruct s f g) -> g (RefStruct s f g))
-                      -> RefStruct s f g -> ST s ()
-modifyRefAt e f = flip modifySTRef f . e . undistinguish . unFixCompose3
+uncomposedly :: (f (g a) -> f' (g' a')) -> Compose f g a -> Compose f' g' a'
+uncomposedly f = Compose . f . getCompose
 
-writeRefAt :: (f (STRef s (g (RefStruct s f g))) -> STRef s (g (RefStruct s f g)))
-                      -> g (RefStruct s f g)
-                      -> RefStruct s f g -> ST s ()
-writeRefAt e v = modifyRefAt e (const v)
+freeze :: (Traversable f) => RefStruct s f -> ST s (Fix f)
+freeze = (newSTRef M.empty >>=) . flip freeze'
+   where
+      freeze' seen struct = do
+         maybeSeen <- M.lookup structID <$> readSTRef seen
+         flip (flip maybe return) maybeSeen $ do
+            frozen <- (Fix <$>) $ traverse (freeze' seen) =<< readSTRef structRef
+            modifySTRef seen (M.insert structID frozen) $> frozen
+         where
+            structID  = identity      unwrapped
+            structRef = undistinguish unwrapped
+            unwrapped = unFixCompose2 struct
 
-readRefAt :: (f (STRef s (g (RefStruct s f g))) -> STRef s (g (RefStruct s f g)))
-                      -> RefStruct s f g
-                      -> ST s (g (RefStruct s f g))
-readRefAt e = readSTRef . e . undistinguish . unFixCompose3
-
-freeze :: (Traversable f, Traversable g) => RefStruct s f g -> ST s (f (g (Fix (Compose f g))))
-freeze = flip (for . undistinguish . unFixCompose3) readSTRef >=>
-         (traverse . traverse $ (Fix . Compose <$>) . freeze)
-
---freeze :: (Traversable f, Traversable g) => RefStruct s f g -> ST s (Fix (Compose f g))
---freeze s = flip freeze' s =<< newSTRef M.empty
---   where 
---      freeze' seen struct = do
---         frozenStruct <- traverse (freeze' seen <=< readSTRef) nakedStruct
---         return frozenStruct
---         where
---            nakedStruct = undistinguish . unFixCompose3 $ struct
-
---data MCons s a b = MCons { mconsID :: Unique , mcar :: STRef s a , mcdr :: STRef s b }
-
---data Cons a b = Cons Unique a b
-
---instance (Show a, Show b) => Show (ConsF (Either a) (Either b)) where
---   show (TreeIn (Cons _ a b)) = "TreeIn (Cons _ (" ++ show a ++ ") (" ++ show b ++ "))"
-
---instance Show (ConsF Maybe Maybe) where
---   show (TreeIn (Cons _ a b)) = "TreeIn (Cons _ " ++ show a ++ " " ++ show b ++ ")"
-
---data TreeFix n l r = TreeIn { treeOut :: n (l (TreeFix n l r))
---                                           (r (TreeFix n l r)) }
-
---type MConsF s l r = TreeFix (MCons s) l r
---type  ConsF   l r = TreeFix  Cons     l r
-
---type MEitherConsF s a b = MConsF s (Either a) (Either b)
---type  EitherConsF   a b =  ConsF   (Either a) (Either b)
-
---mcons :: a -> b -> ST s (MCons s a b)
---mcons a b = MCons <$> newUniqueST <*> newSTRef a <*> newSTRef b
-
---mconsF :: l (MConsF s l r) -> r (MConsF s l r) -> ST s (MConsF s l r)
---mconsF a b = TreeIn <$> mcons a b
-
---consF :: Unique -> l (ConsF l r) -> r (ConsF l r) -> ConsF l r
---consF u a b = TreeIn (Cons u a b)
-
---getCar :: MCons s a b -> ST s a
---getCar = readSTRef . mcar
-
---getCarF :: MConsF s l r -> ST s (l (MConsF s l r))
---getCarF = getCar . treeOut
-
---getCdr :: MCons s a b -> ST s b
---getCdr = readSTRef . mcdr
-
---getCdrF :: MConsF s l r -> ST s (r (MConsF s l r))
---getCdrF = getCdr . treeOut
-
---setCar :: MCons s a b -> a -> ST s ()
---setCar c x = writeSTRef (mcar c) x
-
---setCarF :: MConsF s l r -> l (MConsF s l r) -> ST s ()
---setCarF = setCar . treeOut
-
---setCdr :: MCons s a b -> b -> ST s ()
---setCdr c x = writeSTRef (mcdr c) x
-
---setCdrF :: MConsF s l r -> r (MConsF s l r) -> ST s ()
---setCdrF = setCdr . treeOut
-
---instance Foldable (Either e) where
---   foldMap f (Right m) = f m
---   foldMap _ (Left _)  = mempty
-
---instance Traversable (Either e) where
---   traverse _ (Left e)  = pure (Left e)
---   traverse f (Right x) = Right <$> f x
-
---freeze :: (Traversable f, Traversable g) => MConsF s f g -> ST s (ConsF f g)
---freeze c = flip freeze' c =<< newSTRef M.empty
---   where
---      freeze' seen cell = do
---         maybeSeen <- M.lookup cellID <$> readSTRef seen
---         flip (flip maybe return) maybeSeen $ do
---            l <- traverse (freeze' seen) =<< getCarF cell
---            r <- traverse (freeze' seen) =<< getCdrF cell
---            let frozenCell = consF cellID l r
---            modifySTRef seen (M.insert cellID frozenCell)
---            return frozenCell
---         where cellID = mconsID $ treeOut cell
-
---main :: IO ()
---main = print $ runST $ do
---   x <- mconsF Nothing Nothing
---   --setCdrF x (Right x)
---   freeze x
+test :: IO ()
+test = print $ runST $ do
+   x <- new (Compose ('x',[]))
+   y <- new (Compose ('y',[]))
+   z <- new (Compose ('z',[x,y]))
+   freeze z
