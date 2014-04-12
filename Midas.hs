4cc5d565948761b62ffa6a6d7eb6d7552f1f0e99
Kenneth Foner <kenny.foner@gmail.com>
2014-04-12 14:40:15 -0400

    Moved from ST to IO monads, because I came up with a scenario in which the call to unsafeIOToST really was unsafe (as in, could break equational reasoning).

--- Midas.hs
+++ Midas.hs
@@ -8,7 +8,7 @@ module Midas
    , RefStruct
    , new
    , modify
-   , uncomposedly
+   , composedly
    , freeze
    ) where
 
@@ -21,12 +21,8 @@ import Control.Monad
 import Control.Applicative
 import Control.Arrow
 
-import Control.Monad.ST.Lazy hiding (unsafeIOToST)
-import Control.Monad.ST.Lazy.Unsafe
-import Data.STRef.Lazy
-
-import           Data.Unique (Unique)
-import qualified Data.Unique as U
+import Data.IORef
+import Data.Unique
 
 import           Data.Map (Map)
 import qualified Data.Map as M
@@ -37,30 +33,27 @@ data Distinct x = Distinct
    } deriving ( Eq , Ord )
 
 distinguish :: x -> IO (Distinct x)
-distinguish x = flip Distinct x <$> U.newUnique
+distinguish x = flip Distinct x <$> newUnique
 
-distinguishST :: x -> ST s (Distinct x)
-distinguishST = unsafeIOToST . distinguish
+type RefStruct f = Fix (Compose (Compose Distinct IORef) f)
 
-type RefStruct s f = Fix (Compose (Compose Distinct (STRef s)) f)
+new :: f (RefStruct f) -> IO (RefStruct f)
+new = (Fix . Compose . Compose <$>) . (distinguish =<<) . newIORef
 
-new :: f (RefStruct s f) -> ST s (RefStruct s f)
-new = (Fix . Compose . Compose <$>) . (distinguishST =<<) . newSTRef
-
-modify :: RefStruct s f -> (f (RefStruct s f) -> f (RefStruct s f)) -> ST s ()
-modify = modifySTRef . undistinguish . unFixCompose2
+modify :: RefStruct f -> (f (RefStruct f) -> f (RefStruct f)) -> IO ()
+modify = modifyIORef . undistinguish . unFixCompose2
 
 composedly :: (f (g a) -> f' (g' a')) -> Compose f g a -> Compose f' g' a'
 composedly f = Compose . f . getCompose
 
-freeze :: (Traversable f) => RefStruct s f -> ST s (Fix f)
-freeze = (newSTRef M.empty >>=) . flip freeze'
+freeze :: (Traversable f) => RefStruct f -> IO (Fix f)
+freeze = (newIORef M.empty >>=) . flip freeze'
    where
       freeze' seen struct = do
-         maybeSeen <- M.lookup structID <$> readSTRef seen
+         maybeSeen <- M.lookup structID <$> readIORef seen
          flip (flip maybe return) maybeSeen $ do
-            frozen <- (Fix <$>) $ traverse (freeze' seen) =<< readSTRef structRef
-            modifySTRef seen (M.insert structID frozen) $> frozen
+            frozen <- (Fix <$>) $ traverse (freeze' seen) =<< readIORef structRef
+            modifyIORef seen (M.insert structID frozen) $> frozen
          where
             (structID, structRef) =
                (identity &&& undistinguish) . unFixCompose2 $ struct
