a70ac86d3caf025e0e903ee14b7baa3dca5667c2
Kenneth Foner <kenny.foner@gmail.com>
2014-12-13 18:57:21 -0500

    Wrapped lines to 80 chars for easier viewing.

--- Control/Monad/Squishy.hs
+++ Control/Monad/Squishy.hs
@@ -15,7 +15,8 @@ import Control.Monad.State
 import Control.Monad.Reader
 import Control.Applicative
 
--- | The Squishy monad is a monad with mutable references and optional reference identity
+-- | The Squishy monad is a monad with mutable references and optional reference
+--   identity
 newtype Squishy s a =
    Squishy { getSquishy :: StateT ID (ST s) a }
    deriving ( Functor, Applicative, Monad )
@@ -23,12 +24,12 @@ newtype Squishy s a =
 -- | Runs a Squishy computation, returning a pure value
 runSquishy :: forall a. (forall s. Squishy s a) -> a
 runSquishy x = runST $ evalStateT (getSquishy x) initialID
---runSquishy (Squishy x) = runST $ evalStateT x initialID -- doesn't work, though it should be equivalent
 
 -- | A unique identifier. Only possible to create while making a Distinct value.
 newtype Identifier s = Identifier ID deriving ( Eq, Ord )
 
--- | Data with faked reference equality; the interface provided guarantees that every Distinct value has a unique Identifier.
+-- | Data with faked reference equality; the interface provided guarantees that
+--   every Distinct value has a unique Identifier.
 data Distinct s a =
    Distinct a (Identifier s)
 
@@ -38,7 +39,8 @@ instance Eq (Distinct s a) where
 instance Ord (Distinct s a) where
    compare a b = compare (identify a) (identify b)
 
--- | The only way to create a Distinct value is to generate a new identifier for it in the Squishy monad.
+-- | The only way to create a Distinct value is to generate a new identifier for
+--   it in the Squishy monad.
 distinguish :: a -> Squishy s (Distinct s a)
 distinguish a = Distinct a <$> newIdentifier
    where
@@ -55,7 +57,7 @@ conflate (Distinct a _) = a
 identify :: Distinct s a -> Identifier s
 identify (Distinct _ i) = i
 
--- The mutable reference API from Data.STRef, lifted through to the Squishy monad
+-- The mutable ref API from Data.STRef, lifted through to the Squishy monad
 
 -- | A mutable reference in the @Squishy@ monad.
 newtype Ref s a = Ref (STRef s a)
@@ -76,7 +78,7 @@ writeRef (Ref r) a = Squishy $ lift (writeSTRef r a)
 modifyRef :: Ref s a -> (a -> a) -> Squishy s ()
 modifyRef (Ref r) a = Squishy $ lift (modifySTRef r a)
 
--- This type is not exposed; it's used internally to implement unique identifiers
+-- This type is not exposed, it's used internally to implement unique identifiers
 type ID = Int
 initialID :: ID
 initialID = minBound
--- Data/Kiln.hs
+++ Data/Kiln.hs
@@ -24,10 +24,13 @@ import Control.Arrow
 import           Data.Map ( Map )
 import qualified Data.Map as M
 
--- | A @Clay s f@ is a recursive structure with a uniquely-identified mutable reference at each node. It cannot escape the @Squishy@ monad, by means of the same mechanism as used in @Control.Monad.ST@.
+-- | A @Clay s f@ is a recursive structure with a uniquely-identified mutable
+--   reference at each node. It cannot escape the @Squishy@ monad, by means of
+--   the same mechanism as used in @Control.Monad.ST@.
 data Clay s f = Clay { getClay :: Distinct s (Ref s (f (Clay s f))) }
 
--- | Take a container @f (Clay s f)@, and wrap it in a new mutable reference and distinct tag, returning a new @Clay s f@.
+-- | Take a container @f (Clay s f)@, and wrap it in a new mutable reference and
+--   distinct tag, returning a new @Clay s f@.
 newClay :: f (Clay s f) -> Squishy s (Clay s f)
 newClay = (Clay <$>) . (distinguish =<<) . newRef
 
@@ -35,7 +38,8 @@ newClay = (Clay <$>) . (distinguish =<<) . newRef
 readClay :: Clay s f -> Squishy s (f (Clay s f))
 readClay = readRef . conflate . getClay
 
--- | Use the provided function to destructively update the value at this node of a @Clay s f@.
+-- | Use the provided function to destructively update the value at this node of
+--   a @Clay s f@.
 modifyClay :: Clay s f -> (f (Clay s f) -> f (Clay s f)) -> Squishy s ()
 modifyClay = modifyRef . conflate . getClay
 
@@ -47,7 +51,11 @@ writeClay = writeRef . conflate . getClay
 identifyClay :: Clay s f -> Identifier s
 identifyClay = identify . getClay
 
--- | Given a @Clay s f@, use a natural transformation @(forall a. f a -> g a)@ to convert it into the fixed-point of a the functor @g@ by eliminating the indirection of the mutable references and using the distinct tags on the structure's parts to tie knots where there are cycles in the original graph of references. TThe result is an immutable cyclic lazy data structure.
+-- | Given a @Clay s f@, use a natural transformation @(forall a. f a -> g a)@
+--   to convert it into the fixed-point of a the functor @g@ by eliminating the
+--   indirection of the mutable references and using the distinct tags on the
+--   structure's parts to tie knots where there are cycles in the original graph
+--   of references. TThe result is an immutable cyclic lazy data structure.
 kilnWith :: (Traversable f) => (forall a. f a -> g a) -> Clay s f -> Squishy s (Fix g)
 kilnWith transform = flip evalStateT M.empty . kiln'
    where
@@ -60,10 +68,12 @@ kilnWith transform = flip evalStateT M.empty . kiln'
 kiln :: (Traversable f) => Clay s f -> Squishy s (Fix f)
 kiln = kilnWith id
 
--- | Given a @Squishy@ monad action which returns a @Clay s f@, run the action and use the provided natural transformation during baking (see 'kilnWith').
+-- | Given a @Squishy@ monad action which returns a @Clay s f@, run the action
+--   and use the provided natural transformation during baking (see 'kilnWith').
 runKilningWith :: (Traversable f) => (forall a. f a -> g a) -> (forall s. Squishy s (Clay s f)) -> Fix g
 runKilningWith transform action = runSquishy (action >>= kilnWith transform)
 
--- | Given a @Squishy@ monad action which returns a @Clay s f@, run the action and kiln the result.
+-- | Given a @Squishy@ monad action which returns a @Clay s f@, run the action
+--   and kiln the result.
 runKilning :: (Traversable f) => (forall s. Squishy s (Clay s f)) -> Fix f
 runKilning = runKilningWith id
