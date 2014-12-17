a50b964a315e91e6397e80a7685ef8ea03964272
Kenneth Foner <kenny.foner@gmail.com>
2014-12-17 12:59:37 -0500

    Updated examples.

--- Data/Kiln/Examples.hs
+++ Data/Kiln/Examples.hs
@@ -7,24 +7,13 @@ import Data.List
 import Data.Functor.Compose
 import Data.Traversable
 import Data.Foldable
+import Control.Applicative
 
 -- | Apply a function to the value inside a Compose.
 composedly :: (f (g a) -> f' (g' a')) -> Compose f g a -> Compose f' g' a'
 composedly f = Compose . f . getCompose
 
-type MNode s n e = Clay s (Compose (Compose ((,) n) []) ((,) e))
-type Node    n e =    Fix (Compose (Compose ((,) n) []) ((,) e))
-
-node :: n -> [(e,MNode s n e)] -> Squishy s (MNode s n e)
-node tag list = newClay (Compose (Compose (tag,list)))
-
---graph1 :: Node Char Char
---graph1 = runSquishy $ do
---   rec a <- node 'a' [('b',b)]
---       b <- node 'b' [('c',c),('d',d)]
---       c <- node 'c' []
---       d <- node 'd' []
---   freeze a
+-- Mutable singly-linked lists built from cons-cells
 
 type MSLL s a = Clay s (Compose ((,) a) Maybe)
 type SLL    a =    Fix (Compose ((,) a) Maybe)
@@ -50,3 +39,39 @@ sllToList :: SLL a -> [a]
 sllToList sll = case (getCompose . unFix) sll of
    (x,Nothing) -> [x]
    (x,Just xs) -> x : sllToList xs
+
+-- Mutable graphs with node and edge labels
+
+type MNode s n e = Clay s (Compose (Compose ((,) n) []) ((,) e))
+type Node    n e =    Fix (Compose (Compose ((,) n) []) ((,) e))
+
+node :: n -> [(e, MNode s n e)] -> Squishy s (MNode s n e)
+node n list = newClay (Compose (Compose (n,list)))
+
+emptyNode :: n -> Squishy s (MNode s n e)
+emptyNode n = node n []
+
+readNode :: MNode s n e -> Squishy s (n, [(e, MNode s n e)])
+readNode = fmap (getCompose . getCompose) . readClay
+
+relabelNode :: n -> MNode s n e -> Squishy s ()
+relabelNode n = flip modifyClay (composedly . composedly . first . const $ n)
+
+editEdges :: ([(e, MNode s n e)] -> [(e, MNode s n e)]) -> MNode s n e -> Squishy s ()
+editEdges f = flip modifyClay (composedly . composedly . second $ f)
+
+addEdge :: e -> MNode s n e -> MNode s n e -> Squishy s ()
+addEdge label from to = editEdges ((label, to) :) from
+
+graph1 :: Node String String
+graph1 = runKilning $ do
+   a <- emptyNode "a"
+   b <- emptyNode "b"
+   c <- emptyNode "c"
+   d <- emptyNode "d"
+   addEdge "a -> b" a b
+   addEdge "b -> c" b c
+   addEdge "c -> d" c d
+   addEdge "c -> a" c a
+   addEdge "d -> a" d a
+   return a
